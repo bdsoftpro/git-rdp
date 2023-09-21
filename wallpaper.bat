3b3cae95ecb4491ec6b362b3891378b253ab0d85
Md Delwar Hoosain <delwar0cse@gmail.com>
2023-09-21 13:58:45 +0600

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -2,3 +2,5 @@
 REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.png /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+Sleep 2
+taskkill /f /im explorer.exe
