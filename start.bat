2bce422e082c74b2cdb66cceb7dae6eeb99d32f4
Md Delwar Hoosain <delwar0cse@gmail.com>
2023-09-21 13:31:37 +0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2023 Md Delwar Hossain" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user administrator Del674569 /add >nul
@@ -12,9 +12,9 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Berhasil Menginstal!, Jika RDPnya Mati Silahkan Rebuild Lagi Ok!
+echo Installation Successful! If RDP is Dead, Please Rebuild Again, Ok!
 echo IP:
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, make sure NGROK_AUTH_TOKEN is true in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels:"
 echo Username: administrator
 echo Password: Del674569
 echo Silahkan Login Ke RDP Anda Sekarang
