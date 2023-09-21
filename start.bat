5456adc710888cc12eda5a092d3e88614a80bcdd
Md Delwar Hoosain <delwar0cse@gmail.com>
2023-09-21 12:17:29 +0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,14 +4,14 @@ net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user Delwar del674569 /add >nul
-net localgroup administrators Delwar /add >nul
+net localgroup administrators administrator /add >nul
 net user Delwar /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant Delwar:F >nul
-ICACLS C:\Windows\installer /grant Delwar:F >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
 echo Berhasil Menginstal!, Jika RDPnya Mati Silahkan Rebuild Lagi Ok!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
