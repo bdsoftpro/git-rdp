4f94b129222d21904877977d60d004d2bc7b3b1d
Md Delwar Hoosain <delwar0cse@gmail.com>
2023-09-21 12:27:37 +0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,9 +3,9 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user Delwar del674569 /add >nul
+net user delwar @Del674569 /add >nul
 net localgroup administrators administrator /add >nul
-net user Delwar /active:yes >nul
+net user delwar /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
