902d48ddf8fc91d419098cc5eaf6d37a8fede68c
Md Delwar Hoosain <delwar0cse@gmail.com>
2023-09-21 12:31:14 +0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -10,12 +10,12 @@ net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant delwar:F >nul
+ICACLS C:\Windows\installer /grant delwar:F >nul
 echo Berhasil Menginstal!, Jika RDPnya Mati Silahkan Rebuild Lagi Ok!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
-echo Username: Delwar
-echo Password: del674569
+echo Username: delwar
+echo Password: @Del674569
 echo Silahkan Login Ke RDP Anda Sekarang
 ping -n 10 127.0.0.1 >nul
