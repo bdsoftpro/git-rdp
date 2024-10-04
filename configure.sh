274a9aa332e501fa7697819914c55d95b37950fd
Md Delwar Hoosain <delwar0cse@gmail.com>
2024-10-05 00:05:07 +0600

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,20 +1,20 @@
 #disable spotlight indexing
 sudo mdutil -i off -a
 #Create new account
-sudo dscl . -create /Users/runneradmin
-sudo dscl . -create /Users/runneradmin UserShell /bin/bash
-sudo dscl . -create /Users/runneradmin RealName Runner_Admin
-sudo dscl . -create /Users/runneradmin UniqueID 1001
-sudo dscl . -create /Users/runneradmin PrimaryGroupID 80
-sudo dscl . -create /Users/runneradmin NFSHomeDirectory /Users/tcv
-sudo dscl . -passwd /Users/runneradmin Del674569
-sudo dscl . -passwd /Users/runneradmin Del674569
-sudo createhomedir -c -u runneradmin > /dev/null
-sudo dscl . -append /Groups/admin GroupMembership runneradmin
+sudo dscl . -create /Users/delwar
+sudo dscl . -create /Users/delwar UserShell /bin/bash
+sudo dscl . -create /Users/delwar RealName "VNC REAL"
+sudo dscl . -create /Users/delwar UniqueID 1001
+sudo dscl . -create /Users/delwar PrimaryGroupID 80
+sudo dscl . -create /Users/delwar NFSHomeDirectory /Users/tcv
+sudo dscl . -passwd /Users/delwar $1
+sudo dscl . -passwd /Users/delwar $1
+sudo createhomedir -c -u delwar > /dev/null
+sudo dscl . -append /Groups/admin GroupMembership delwar
 #Enable VNC
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
-echo runnerrdp | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/4djOsU1JWf5NzgbPKxlpXq2836wnVZkH7/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvncpw -vncpw $2
 #Start VNC/reset changes
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
@@ -25,4 +25,4 @@ unzip osx-64.zip
 sudo chmod +x ./localtonet
 
 #configure ngrok and start it
-./localtonet authtoken 4djOsU1JWf5NzgbPKxlpXq2836wnVZkH7 &
+./localtonet authtoken $3 &
