8f94e1d803de25a090885b100230f68d1102723b
Md Delwar Hoosain <delwar0cse@gmail.com>
2024-10-07 00:09:51 +0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,6 +2,7 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2023 Md Delwar Hossain" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net user %1 %2 /add >nul
 net localgroup administrators %1 /add >nul
 net user %1 /active:yes >nul
